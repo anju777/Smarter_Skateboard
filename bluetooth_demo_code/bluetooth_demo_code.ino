6756be61d3254fef25c8d29a59e1dd05a0c903f8
emmaline01 <emmaline.mai@gmail.com>
2022-02-11 11:35:12 -0800

    untested code for controlling ESC via bluetooth commands

--- bluetooth_demo_code/bluetooth_demo_code.ino
+++ bluetooth_demo_code/bluetooth_demo_code.ino
@@ -1,16 +1,27 @@
+#include <Servo.h>
 #include <SoftwareSerial.h>
 SoftwareSerial HM10(2, 3); // RX = 2, TX = 1
 
+// https://bluerobotics.com/learn/guide-for-controlling-the-basic-esc-with-a-potentiometer/#:~:text=Connecting%20the%20ESC&text=Connect%20the%20white%20signal%20wire,ESC%20to%20your%20power%20source.
+#define ESC_PIN 9; // signal pin for the ESC
+Servo esc;
+
 char appData;  
 String inData = "";
+int speed = 100; // TODO: tune
 
 void setup() {
-  // put your setup code here, to run once:
+  // set up bluetooth module
   Serial.begin(9600);
   Serial.println("HM10 serial started at 9600");
   HM10.begin(9600); // set HM10 serial at 9600 baud rate
   pinMode(13, OUTPUT); // onboard LED
   digitalWrite(13, LOW); // switch OFF LED
+
+  //set up servo
+  esc.attach(ESC_PIN);
+  esc.writeMicroseconds(1500); // send "stop" signal to ESC. Also necessary to arm the ESC. servo.write(0)?? https://forum.arduino.cc/t/arduino-and-esc-and-motor/563422/8
+  delay(7000); // delay to allow the ESC to recognize the stopped signal.
 }
 
 void loop() {
@@ -19,5 +30,27 @@ void loop() {
       appData = HM10.read();
       inData = String(appData);  // save the data in string format
       Serial.write(appData);
+
+      if (appData == ' ') {
+        fwd();
+      }
+      else if (appData == 'q') {
+        rev();
+      }
+      else {
+        brake();
+      }
     }
+}
+
+void fwd() {
+  esc.write(speed);
+}
+
+void rev() {
+  esc.write(-speed);
+}
+
+void brake() {
+  esc.write(90);
 }
